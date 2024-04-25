c1f9f200e10ca5b219758ad5d03e06f592ffd016
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-25 04:19:07 -0400

    Update model.py

--- model.py
+++ model.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-from torch.utils.data import datasets, DataLoader
+from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
 
