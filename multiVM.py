d3fa272a610f3a0df4871550a4d2c82b8673a1c6
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-25 05:55:04 -0400

    Update multiVM.py

--- multiVM.py
+++ multiVM.py
@@ -78,7 +78,7 @@ class Trainer:
 
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
-        print(f"[RANK{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        print(f"[RANK{self.global_rank}] [LOCAL_RANK{self.local_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
         self.train_data.sampler.set_epoch(epoch)
         for inputs, labels in self.train_data:
             # inputs = inputs.to(self.local_rank)
