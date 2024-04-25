fbc5498578857d49c993039219dc24aeea730e81
Hardwood Vazquez <s.vazquez6363@gmail.com>
2024-04-25 03:58:13 -0400

    create model.py

---
+++ model.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+
+
+# Define the model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class Trainer:
+    def __init__(
+        self,
+        train_data: DataLoader,
+        optimizer: optim.Optimizer,
+        save_every: int, 
+    ) -> None:
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+
+    def _run_batch(self, inputs, labels):
+        self.optimizer.zero_grad()
+        output = self.model(inputs)
+        loss = F.cross_entropy(output, labels)
+        loss.backward()
+        self.optimizer.step()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(f"Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        for inputs, labels in self.train_data:
+            inputs = inputs.to(self.gpu_id)
+            labels = labels.to(self.gpu_id)
+            self._run_batch(inputs, labels)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.state_dict()
+        PATH = "checkpoint.pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if epoch % self.save_every == 0:
+                self._save_checkpoint(epoch)
+
+
+
+def load_train_objs():
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_set = Dataset.CIFAR10(root='./data', train=True, download=True, transform=transform)  # load your dataset
+    model = Net()  # load your model
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    return train_set, model, optimizer
+
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=2,
+        shuffle=True,
+    )
+
+
+def main(total_epochs, save_every, batch_size):
+    dataset, optimizer = load_train_objs()
+    train_data = prepare_dataloader(dataset, batch_size)
+    trainer = Trainer(train_data, optimizer, save_every)
+    trainer.train(total_epochs)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='simple distributed training job')
+    parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
+    parser.add_argument('save_every', type=int, help='How often to save a snapshot')
+    parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each default: 32)')
+    args = parser.parse_args()
+    
+    main(args.total_epochs, args.save_every, args.batch_size)
